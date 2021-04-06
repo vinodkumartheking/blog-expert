f56aa462b869c2d4b72a5158f305fbc49bc9f368
unknown <vinodkumartheking@gmail.com>
2021-04-06 11:28:32 +0530

    added few files and modules

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+  
+// Static Middleware
+//app.use(express.static(path.join(__dirname, 'public')))
+var public = path.join(__dirname, 'public');
+
+
+  
+// View Engine Setup
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
+  
+app.get('/', function(req, res) {
+    res.sendFile(path.join(public, 'index.html'));
+});
+
+app.use('/', express.static(public));
+  
+app.listen(8080, function(error){
+    if(error) throw error
+    console.log("Server created Successfully")
+})
